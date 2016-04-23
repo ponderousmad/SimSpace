9a2cfee81a7e48ce9e0234222cb67e76abb96c20
Adrian Smith <agnomen@gmail.com>
2016-04-23 00:47:23 -0400

    Parse MOLA data.

--- Assets/scripts/LandscapeGenerator.cs
+++ Assets/scripts/LandscapeGenerator.cs
@@ -5,19 +5,24 @@ using System.Collections;
 public class LandscapeGenerator : MonoBehaviour {
 
 	public float northSpan = 10;
-	public float westSpan = 10;
+	public float eastSpan = 10;
+
+	public float northStart = 0;
+	public float eastStart = 0;
 
 	public int northSegments = 100;
-	public int westSegments = 100;
+	public int eastSegments = 100;
+
+	public TextAsset altitudeData;
 
 	private Mesh mMesh = null;
 
 	private int VertexCount {
-		get { return (1 + northSegments) * (1 + westSegments); }
+		get { return (1 + northSegments) * (1 + eastSegments); }
 	}
 
 	private int TriangleCount {
-		get { return (northSegments * westSegments * 2); }
+		get { return (northSegments * eastSegments * 2); }
 	}
 
 	[ContextMenu ("Generate")]
@@ -44,6 +49,67 @@ public class LandscapeGenerator : MonoBehaviour {
 		}
 		Debug.Log ("Generating Mesh");
 
+		var eastStep = eastSpan / eastSegments;
+		var northStep = northSpan / northSegments;
+
+		var heightSum = new float[northSegments + 1, eastSegments + 1];
+		var heightCount = new int[northSegments + 1, eastSegments + 1];
+
+		var count = 0;
+		var minEast = float.MaxValue;
+		var maxEast = float.MinValue;
+		var minNorth = float.MaxValue;
+		var maxNorth = float.MinValue;
+		var minHeight = float.MaxValue;
+		var maxHeight = float.MinValue;
+
+		foreach (var line in altitudeData.text.Split('\r')) {
+			++count;
+
+			if (count > 1) {
+				var parts = line.Split(',');
+				float east, north, height;
+				if (!float.TryParse (parts [0].Trim (), out east)) {
+					Debug.Log ("Could not parse: " + parts [0] + " at " + count);
+					break;
+				}
+				if (!float.TryParse (parts [1].Trim (), out north)) {
+					Debug.Log ("Could not parse: " + parts [1] + " at " + count);
+					break;
+				}
+				if (!float.TryParse (parts [2].Trim (), out height)) {
+					Debug.Log ("Could not parse: " + parts [2] + " at " + count);
+					break;
+				}
+				// Debug.Log (string.Format("{0}, {1}, {2}", east, north, height));
+
+				minEast = Mathf.Min (minEast, east);
+				maxEast = Mathf.Max (maxEast, east);
+				minNorth = Mathf.Min (minNorth, north);
+				maxNorth = Mathf.Max (maxNorth, north);
+				minHeight = Mathf.Min (minHeight, height);
+				maxHeight = Mathf.Max (maxHeight, height);
+
+				if (east < eastStart || eastStart + eastSpan + eastStep < east) {
+					continue;
+				}
+
+				if (north < northStart || northStart + northSpan + northStep < north) {
+					continue;
+				}
+
+				var x = (int) ((east - eastStart) / eastStep);
+				var y = (int) ((north - northStart) / northStep);
+				heightSum [x, y] += height;
+				heightCount [x, y] += 1;
+			}
+		}
+
+		Debug.Log ("Lines: " + count);
+		Debug.Log (string.Format ("{0} - {1}", minEast, maxEast - minEast));
+		Debug.Log (string.Format ("{0} - {1}", minNorth, maxNorth - minNorth));
+		Debug.Log (string.Format ("{0} - {1}", minHeight, maxHeight - minHeight));
+
 		var mf = GetComponent<MeshFilter>();
 		mMesh = new Mesh();
 		mf.mesh = mMesh;
@@ -53,16 +119,19 @@ public class LandscapeGenerator : MonoBehaviour {
 		var normals = new Vector3[VertexCount];
 		var uv = new Vector2[VertexCount];
 
-		var westStep = westSpan / westSegments;
-		var northStep = northSpan / northSegments;
-		var uStep = 1.0f / westSegments;
+		var uStep = 1.0f / eastSegments;
 		var vStep = 1.0f / northSegments;
 		var index = 0;
 		var triIndex = 0;
-		for (int x = 0; x <= westSegments; ++x) {
-			var generateTris = x < westSegments;
+		float lastHeight = 0;
+		for (int x = 0; x <= eastSegments; ++x) {
+			var generateTris = x < eastSegments;
 			for (int y = 0; y <= northSegments; ++y) {
-				vertices [index] = new Vector3 (x * westStep, 0, y * northStep);
+				var heights = heightCount [x, y];
+				if (heights > 0) {
+					lastHeight = heightSum [x, y] / (heights * 1000.0f);
+				}
+				vertices [index] = new Vector3 (x * eastStep, lastHeight, y * northStep);
 				normals [index] = Vector3.up;
 				uv [index] = new Vector2 (x * uStep, y * vStep);
 
