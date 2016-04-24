a5d3d21ff931c96ce914d9f48ebe470e193c7272
Adrian Smith <agnomen@gmail.com>
2016-04-23 20:15:30 -0400

    Storm start delay.

---
+++ Assets/scripts/Storm.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Storm : MonoBehaviour {
+
+	public float startDelay = 30.0f;
+	public float rampTime = 25.0f;
+	public float fogDensity = 0.1f;
+
+	private bool mStarted = false;
+	private float mElapsed = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mStarted && mElapsed < rampTime) {
+			mElapsed = Mathf.Min (mElapsed + Time.deltaTime, rampTime);
+			RenderSettings.fogDensity = fogDensity * mElapsed / rampTime;
+		} else if (!mStarted) {
+			mElapsed += Time.deltaTime;
+			if (mElapsed > startDelay) {
+				StartStorm ();
+			}
+		}
+	}
+
+	public void StartStorm() {
+		mStarted = true;
+		mElapsed = 0.0f;
+
+		var particles = GetComponent<ParticleSystem>();
+		if (particles != null) {
+			particles.Play ();
+		}
+	}
+}
