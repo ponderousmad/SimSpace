f0c2a83bd910ec521081f64bdba062bdb7c5055a
Adrian Smith <agnomen@gmail.com>
2016-04-24 02:20:54 -0400

    Dim the lights when the storm starts.

--- Assets/scripts/Storm.cs
+++ Assets/scripts/Storm.cs
@@ -6,6 +6,8 @@ public class Storm : MonoBehaviour {
 	public float startDelay = 30.0f;
 	public float rampTime = 25.0f;
 	public float fogDensity = 0.1f;
+	public float dim = 0.25f;
+	public Light daylight;
 
 	private bool mStarted = false;
 	private float mElapsed = 0;
@@ -19,6 +21,9 @@ public class Storm : MonoBehaviour {
 		if (mStarted && mElapsed < rampTime) {
 			mElapsed = Mathf.Min (mElapsed + Time.deltaTime, rampTime);
 			RenderSettings.fogDensity = fogDensity * mElapsed / rampTime;
+			if (daylight != null) {
+				daylight.intensity = 1 - (dim * mElapsed / rampTime);
+			}
 		} else if (!mStarted) {
 			mElapsed += Time.deltaTime;
 			if (mElapsed > startDelay) {
