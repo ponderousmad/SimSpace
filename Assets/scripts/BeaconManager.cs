59c6614ad8aa5379c9f3d84f0bca508d540ca4fd
Adrian Smith <agnomen@gmail.com>
2016-04-24 09:22:32 -0400

    Make beacons collectable.

--- Assets/scripts/BeaconManager.cs
+++ Assets/scripts/BeaconManager.cs
@@ -6,11 +6,14 @@ public class BeaconManager : MonoBehaviour {
 
 	public List<AudioClip> tones = new List<AudioClip>();
 	public float period = 5.0f;
+	public float collectDistance = 2.0f;
+	public float keepDistance = 5.0f;
 	public GameObject beaconPrefab;
 
 	private float mTimer = 0;
 	private List<AudioSource> mSources = new List<AudioSource>();
 	private int mBeaconIndex = 0;
+	private bool mCanRemove = false;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +22,23 @@ public class BeaconManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		AudioSource toRemove = null;
+
+		foreach (var source in mSources) {
+			if (Vector4.Distance (source.transform.position, transform.position) < collectDistance) {
+				toRemove = source;
+			}
+		}
+
+		if (mSources.Count > 0) {
+			if (Vector4.Distance(mSources[mSources.Count - 1].transform.position, transform.position) > keepDistance) {
+				mCanRemove = true;
+			}
+			if (mCanRemove && toRemove != null) {
+				mSources.Remove (toRemove);
+			}
+		}
+
 		mTimer += Time.deltaTime;
 		if (mTimer > period) {
 			mTimer -= period;
@@ -39,5 +59,6 @@ public class BeaconManager : MonoBehaviour {
 		source.clip = tones [mBeaconIndex];
 		mSources.Add (source);
 		mBeaconIndex = (mBeaconIndex + 1) % tones.Count;
+		mCanRemove = false;
 	}
 }
