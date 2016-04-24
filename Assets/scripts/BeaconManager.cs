6a89759780ae2754fa04cd51d884d0f36ef64c3d
Adrian Smith <agnomen@gmail.com>
2016-04-24 01:50:43 -0400

    Hook up beacons.

--- Assets/scripts/BeaconManager.cs
+++ Assets/scripts/BeaconManager.cs
@@ -6,7 +6,7 @@ public class BeaconManager : MonoBehaviour {
 
 	public List<AudioClip> tones = new List<AudioClip>();
 	public float period = 5.0f;
-	public AudioSource sourcePrefab;
+	public GameObject beaconPrefab;
 
 	private float mTimer = 0;
 	private List<AudioSource> mSources = new List<AudioSource>();
@@ -27,10 +27,15 @@ public class BeaconManager : MonoBehaviour {
 				source.Play ();
 			}
 		}
+
+		if (Input.GetKeyDown ("space")) {
+			CreateBeacon (this.transform);
+		}
 	}
 
 	void CreateBeacon(Transform at) {
-		var source = Instantiate (sourcePrefab, at.position, at.rotation) as AudioSource;
+		var beacon = Instantiate (beaconPrefab, at.position, at.rotation) as GameObject;
+		var source = beacon.GetComponentInChildren<AudioSource> ();
 		source.clip = tones [mBeaconIndex];
 		mSources.Add (source);
 		mBeaconIndex = (mBeaconIndex + 1) % tones.Count;
