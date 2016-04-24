3ee8007cc81aabd8c0f54f0c0f2da89bca578009
MacyK <macy.kuang@gmail.com>
2016-04-23 20:43:07 -0400

    add dynamic level generating

---
+++ Assets/scripts/GenerateBlock.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+using System.Collections;
+
+public class GenerateBlock : MonoBehaviour
+{
+    public Direction generateDirection;
+    public GameObject targetParent;
+    bool mTriggered;
+
+    public enum Direction
+    {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("other name: " + other.name);
+        if(string.Equals("ColliderBody", other.name) && LevelBockGenerate.instance && !mTriggered)
+        {
+            Debug.Log("Create");
+            mTriggered = true;
+            LevelBockGenerate.instance.GenerateNewBlock(targetParent, generateDirection);
+        }
+    }
+}
---
+++ Assets/scripts/LevelBockGenerate.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelBockGenerate : MonoBehaviour
+{
+    private static LevelBockGenerate mInstance;
+    public static LevelBockGenerate instance { get { return mInstance; } }
+    public GameObject blockPrefab;
+    public float blockSize = 990;
+
+    void Start()
+    {
+        if (mInstance == null)
+        {
+            mInstance = this;
+        }
+    }
+
+    public void GenerateNewBlock(GameObject target, GenerateBlock.Direction direction)
+    {
+        GameObject block = (GameObject)Instantiate(blockPrefab);
+        Vector3 addPos = Vector3.zero;
+        Vector3 targetPos = target.transform.position;
+        Debug.Log("GenerateNewBlock");
+        switch(direction)
+        {
+            case GenerateBlock.Direction.FORWARD:
+                addPos = new Vector3(0, 0, blockSize);
+                break;
+            case GenerateBlock.Direction.BACKWARD:
+                addPos = new Vector3(0, 0, -blockSize);
+                break;
+            case GenerateBlock.Direction.LEFT:
+                addPos = new Vector3(-blockSize, 0, 0);
+                break;
+            case GenerateBlock.Direction.RIGHT:
+                addPos = new Vector3(blockSize, 0, 0);
+                break;
+        }
+
+        block.transform.position = targetPos + addPos;
+    }
+}
